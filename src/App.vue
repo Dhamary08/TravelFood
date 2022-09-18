456194a1eac4aedad93c696be7fc5af35a3362a8
Valeria Manriquez <vmanriquez@sourcing.cl>
2022-09-18 13:56:02 -0300

    base

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,31 @@
     <header-nav @open-view="changeActiveViews" />
     <div class="container-fluid">
       <div class="row">
-        <main class="col-10">
+        <main class="col-12">
           <div v-if="activeViewName === 'login'" class="mt-5">
             <login-users></login-users>
           </div>
           <div v-if="activeViewName === 'register'" class="mt-5">
             <register-users></register-users>
           </div>
+          <div v-if="activeViewName === 'products'" class="mt-5">
+            <my-products></my-products>
+          </div>
+          <div v-if="activeViewName === 'shoppingCart'" class="mt-5">
+            <shopping-cart></shopping-cart>
+          </div>
         </main>
       </div>
     </div>
-    <footer-nav />
+    <footer-nav class="footer"/>
   </div>
 </template>
 <script>
 import HeaderNav from './components/layout/Header-nav.vue';
 import LoginUsers from './components/registro/LoginUsers.vue';
 import RegisterUsers from './components/registro/RegisterUsers.vue';
+import MyProducts from './components/home/MyProducts.vue';
+import ShoppingCart from './components/home/ShoppingCart.vue';
 import FooterNav from './components/layout/FooterNav.vue';
 
 export default {
@@ -27,6 +35,8 @@ export default {
     HeaderNav,
     LoginUsers,
     RegisterUsers,
+    MyProducts,
+    ShoppingCart,
     FooterNav,
   },
   data() {
@@ -42,3 +52,10 @@ export default {
   },
 };
 </script>
+<style>
+.footer {
+  position: fixed;
+  bottom: 0px;
+  width: 100% ;
+}
+</style>
