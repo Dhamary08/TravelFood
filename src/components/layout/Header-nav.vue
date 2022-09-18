e764f81350c12dfbbaf0d1103cb8a5a660a44bdb
Valeria Manriquez <vmanriquez@sourcing.cl>
2022-09-18 13:41:43 -0300

    base

--- src/components/layout/Header-nav.vue
+++ src/components/layout/Header-nav.vue
@@ -4,7 +4,16 @@
       <b-navbar-brand href="#">Travel Food</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav> </b-navbar-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#" active @click="view('products')">
+            <b-icon icon="person-circle" aria-hidden="true"></b-icon>
+            Productos
+          </b-nav-item>
+          <b-nav-item href="#" active @click="view('shoppingCart')">
+            <b-icon icon="person-circle" aria-hidden="true"></b-icon>
+            Carro de Compra
+          </b-nav-item>
+        </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-item href="#" active @click="view('login')">
             <b-icon icon="person-circle" aria-hidden="true"></b-icon>
